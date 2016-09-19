94a01090ba8e0935b4e776508ca5fb78435b6b36
Idaho Frost <frostidaho@gmail.com>
2016-09-19 18:51:30 -0400

    Set table max_rows to six in simple.show*

--- src/gpopup/simple.py
+++ src/gpopup/simple.py
@@ -8,7 +8,7 @@ def show(summary='', body='', table='', table_cols=(),
     client = _noti.NotifierClient()
     client.start_server_maybe()
     pre_msg = _mtypes.Simple(summary=summary, body=body)
-    table_msg = _mtypes.Table(body=table, columns=table_cols)
+    table_msg = _mtypes.Table(body=table, columns=table_cols, max_rows=6)
     post_msg = _mtypes.Simple(summary=post_summary, body=post_body)
     n_id = client.new_window(pre_msg, table_msg, post_msg, position=position)
     client.timeout(timeout=display_time, notification_id=n_id)
@@ -20,7 +20,7 @@ def show_blocking(summary='', body='', table='', table_cols=(),
          position='northeast'):
     import gpopup.message_widgets as _mwidgets
     pre_msg = _mtypes.Simple(summary=summary, body=body)
-    table_msg = _mtypes.Table(body=table, columns=table_cols)
+    table_msg = _mtypes.Table(body=table, columns=table_cols, max_rows=6)
     post_msg = _mtypes.Simple(summary=post_summary, body=post_body)
     win = _mwidgets.MainWindow(pre_msg, table_msg, post_msg)
     win.position = position
